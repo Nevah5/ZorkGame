e2a1cc23611b390b2a763356ee31c83bb77b7d4b
Nevah5 <neevaah5@gmail.com>
2023-01-16 16:55:15 +0100

    Update HelpCommand with an example

--- src/main/java/dev/nevah5/zorkgame/commands/HelpCommand.java
+++ src/main/java/dev/nevah5/zorkgame/commands/HelpCommand.java
@@ -2,6 +2,11 @@ package dev.nevah5.zorkgame.commands;
 
 public class HelpCommand {
     public HelpCommand(){
-        System.out.println("HELP Command WIP");
+        System.out.println("- Mᴏᴠᴇᴍᴇɴᴛ -");
+        System.out.println("map - prints out the map and your location");
+        System.out.println("move [n(orth)/e(ast)/s(outh)/w(est)] - moves the player into the desired direction");
+        System.out.println("search - search in the player's location\n");
+        System.out.println("- Iɴᴠᴇɴᴛᴏʀʏ -");
+        System.out.println("inventory - lists the player's inventory");
     }
 }
